0c077c196a09413bba8a0536b1df67b912c802c7
Anastasiya Zaitsava <5583598@mail.ru>
2021-12-14 18:54:11 +0300

    added task 1

---
+++ index.js
@@ -0,0 +1,12 @@
+// Task 1
+let str="Привет";
+console.log (typeof str, "from to Task 1");
+
+let num=123;
+console.log (typeof num, "from to Task 1");
+
+let flag=true;
+console.log (typeof flag, "from to Task 1");
+
+let txt="true";
+console.log (typeof txt, "from to Task 1");
