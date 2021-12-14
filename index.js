abab7a791f4635afee30b4d2a99d29f086e3c99a
Anastasiya Zaitsava <5583598@mail.ru>
2021-12-14 19:13:30 +0300

    added task 4

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
 // Task 1
+console.log("Task 1");
+
 let str="Привет";
-console.log (typeof str);
+console.log(typeof str);
 
 let num=123;
-console.log (typeof num);
+console.log(typeof num);
 
 let flag=true;
-console.log (typeof flag);
+console.log(typeof flag);
 
 let txt="true";
-console.log (typeof txt);
+console.log(typeof txt);
 
 // Task 2
+console.log("Task 2");
+
 let a1=5+3;
-console.log (a1, "from to Task 2");
+console.log(a1);
 
 let a2=5-3;
-console.log (a2, "from to Task 2");
+console.log(a2);
 
 let a3=5*3;
-console.log (a3, "from to Task 2");
+console.log(a3);
 
 let a4=5/3;
-console.log (a4, "from to Task 2");
+console.log(a4);
+
+// Task 3
+console.log("Task 3");
+
+let a5=5%3;
+console.log(a5);
+
+let a6=3%5;
+console.log(a6);
+
+let a7=5+"3";
+console.log(a7);
+
+let a8="5"-3;
+console.log(a8);
+
+let a9=75+"кг";
+console.log(a9);
+
+// Task 4
+console.log("Task 4");
+
+let heigh=23;
+let width=10;
+let sm="см";
+let S=heigh*width+sm;
+console.log(S);
