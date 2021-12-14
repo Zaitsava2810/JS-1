d4ba4115e9a3719e8c44d7c29ef424eb61228797
Anastasiya Zaitsava <5583598@mail.ru>
2021-12-14 18:58:02 +0300

    added task 2

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 // Task 1
 let str="Привет";
-console.log (typeof str, "from to Task 1");
+console.log (typeof str);
 
 let num=123;
-console.log (typeof num, "from to Task 1");
+console.log (typeof num);
 
 let flag=true;
-console.log (typeof flag, "from to Task 1");
+console.log (typeof flag);
 
 let txt="true";
-console.log (typeof txt, "from to Task 1");
+console.log (typeof txt);
+
+// Task 2
+let a1=5+3;
+console.log (a1, "from to Task 2");
+
+let a2=5-3;
+console.log (a2, "from to Task 2");
+
+let a3=5*3;
+console.log (a3, "from to Task 2");
+
+let a4=5/3;
+console.log (a4, "from to Task 2");
